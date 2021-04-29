c283883ca9408a8ef8fbf425d25e7f6d0e9ef404
Chelsea Li <chelseali2001@gmail.com>
2021-04-29 16:39:10 -0700

    created more test cases

--- calculator.py
+++ calculator.py
@@ -1,11 +1,26 @@
 def add(a, b):
+    if (type(a) is str or type(b) is str):
+        return "Numbers only"
+
     return a + b
 
 def sub(a, b):
+    if (type(a) is str or type(b) is str):
+        return "Numbers only"
+
     return a - b
 
 def mul(a, b):
+    if (type(a) is str or type(b) is str):
+        return "Numbers only"
+
     return a * b
 
 def div(a, b):
+    if (type(a) is str or type(b) is str):
+        return "Numbers only"
+        
+    if b == 0:
+        return "0 can't be the divisor"
+
     return a / b
---
+++ test_calculator.py
@@ -0,0 +1,39 @@
+import unittest
+import calculator
+
+class SimpleTest(unittest.TestCase):
+    def test_calculator(self):
+        self.assertEqual(calculator.add(4, 2), 6) 
+        
+    def test_calculator0(self):
+        self.assertEqual(calculator.sub(4, 2), 2)
+    
+    def test_calculator1(self):
+        self.assertEqual(calculator.mul(4, 2), 8)
+
+    def test_calculator2(self):
+        self.assertEqual(calculator.div(4, 2), 2.0)
+    
+    def test_calculator3(self):
+        self.assertEqual(calculator.add(4, -2), 2) 
+        
+    def test_calculator4(self):
+        self.assertEqual(calculator.sub(4.5, 2), 2.5)
+    
+    def test_calculator5(self):
+        self.assertEqual(calculator.mul(4, -2.5), -10)
+
+    def test_calculator6(self):
+        self.assertEqual(calculator.div(4, -2), -2.0)
+    
+    def test_calculator7(self):
+        self.assertEqual(calculator.div(4, 0), "0 can't be the divisor")
+
+    def test_calculator8(self):
+        self.assertEqual(calculator.add('4', 2), "Numbers only")
+    
+    def test_calculator9(self):
+        self.assertEqual(calculator.sub(4, 2), 10)
+
+if __name__ == '__main__':
+	unittest.main()
